7269aa1d3ead24503c3f18cf1aef79de59297e2c
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2024-04-29 12:42:21 +0200

    Update events-script.js

--- events-script.js
+++ events-script.js
@@ -6,7 +6,7 @@ var arEventi = [
       luogo: 'Case di Coccia - Folignano (AP)',
       link: ''
    },
-  {
+   {
       nome: 'Sagra della Tagliata',
       data: '18/07/2024',
       luogo: 'Piane di Morro - Folignano (AP)',
@@ -17,24 +17,36 @@ var arEventi = [
 // Funzione per popolare gli eventi nel documento HTML
 function popolaEventi() {
    var eventContainer = document.getElementById('event-container');
-   var eventTemplate = document.getElementById('event-template');
 
    // Per ogni evento nell'array
    arEventi.forEach(function(evento) {
-      // Clona il template
-      var clone = eventTemplate.content.cloneNode(true);
+      // Creiamo la card dell'evento
+      var card = document.createElement('div');
+      card.classList.add('card', 'link');
+      card.style.flex = '0 0 80%';
+      card.style.padding = '10px';
+      card.style.maxWidth = '100%';
+      card.style.margin = '0';
+      card.style.display = 'flex';
+      card.style.alignItems = 'center';
+      card.style.backgroundColor = '#ffffff22';
 
-      // Popola i dati dell'evento nel clone
-      clone.querySelector('.event-name').textContent = evento.nome;
-      clone.querySelector('.event-location').textContent = evento.luogo;
+      // Costruiamo il contenuto della card dell'evento
+      var dataEvento = new Date(evento.data);
+      card.innerHTML = `
+         <div style="flex: 1; text-align: center; padding:0 5px;">
+            <div style="font-size: 20px;">${dataEvento.toLocaleDateString('it-IT', { weekday: 'short' }).toUpperCase()}</div>
+            <div style="font-size: 34px; font-weight: bold;">${dataEvento.getDate()}</div>
+            <div style="font-size: 20px;">${dataEvento.toLocaleDateString('it-IT', { month: 'short' }).toUpperCase()}</div>
+         </div>
+         <div style="flex: 7; text-align: center;">
+            <div style="font-size: 20px; font-weight: bold; text-transform: uppercase">${evento.nome}</div>
+            <div style="font-size: 16px;">${evento.luogo}</div>
+         </div>
+      `;
 
-      var data = new Date(evento.data);
-      clone.querySelector('.event-day').textContent = data.getDate();
-      clone.querySelector('.event-month').textContent = data.toLocaleString('default', { month: 'short' }).toUpperCase();
-      clone.querySelector('.event-year').textContent = data.getFullYear();
-
-      // Aggiungi il clone al container degli eventi
-      eventContainer.appendChild(clone);
+      // Aggiungiamo la card dell'evento al container degli eventi
+      eventContainer.appendChild(card);
    });
 }
 
