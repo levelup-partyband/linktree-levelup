506ad5fd98f6ca90c3e6a2bec89b4ba3010eb87c
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2024-04-29 15:16:05 +0200

    Update events-script.js

--- scripts/events-script.js
+++ scripts/events-script.js
@@ -21,7 +21,7 @@ function popolaEventi() {
         if (!isNaN(data.getTime()) && (data >= oggi || data.getDate() === oggi.getDate())) {
             // Costruisci direttamente il template CARD utilizzando la sintassi dei template string
             var cardHTML = `
-               div class="card link" style="flex:0 0 80%; padding: 10px; max-width: 100%; margin:0; display: flex; align-items:center; background-color:#ffffff22; cursor: ${evento.link ? 'pointer' : 'default'}" onclick="apriLink('${evento.link}')">
+               <div class="card link" style="flex:0 0 80%; padding: 10px; max-width: 100%; margin:0; display: flex; align-items:center; background-color:#ffffff22; cursor: ${evento.link ? 'pointer' : 'default'}" onclick="apriLink('${evento.link}')">
                  <div style="flex: 1; text-align: center; padding:0 5px;">
                      <div class="event-day" style="font-size: 20px;">${data.toLocaleString('default', { weekday: 'short' }).toUpperCase()}</div>
                      <div class="event-date" style="font-size: 34px; font-weight: bold;">${data.getDate()}</div>
@@ -30,6 +30,7 @@ function popolaEventi() {
                  <div style="flex: 7; text-align: center;">
                      <div class="event-name" style="font-size: 20px; font-weight: bold; text-transform: uppercase;">${evento.nome}</div>
                      <div class="event-location" style="font-size: 16px;">${evento.luogo}</div>
+                     ${evento.indirizzo ? `<div class="event-address" style="font-size: 12px;">${evento.indirizzo}</div>` : ''}
                  </div>
              </div>
             `;
