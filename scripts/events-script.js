464c42a9aad151e1304cf1c93bab11f5fcb6c2e6
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2024-05-06 14:25:45 +0200

    Update events-script.js

--- scripts/events-script.js
+++ scripts/events-script.js
@@ -3,7 +3,10 @@ var arEventi = [
    { nome: "Festa di Sant'Antonio", data: '15/06/2024', luogo: 'Spelonga (AP)', link: '', indirizzo: '', indirizzoLink: '' },
    { nome: 'Festa di Case di Coccia', data: '29/06/2024', luogo: 'Case di Coccia - Folignano (AP)', link: '', indirizzo: 'Piazza di Via Ivrea, Case di Coccia (AP)', indirizzoLink: 'https://maps.app.goo.gl/Te7qTcCRs8eUL9B79' },
    { nome: 'Sagra della Tagliata', data: '18/07/2024', luogo: 'Piane di Morro - Folignano (AP)', link: '', indirizzo: 'Piazza Giovanni Paolo II, Piane di Morro (AP)', indirizzoLink: 'https://maps.app.goo.gl/o8VwZFkG4ZLWJ1qB9' },
+   { nome: 'Piazzatta e Campari', data: '01/08/2024', luogo: "Sant'Egidio di Monsampolo", link: '', indirizzo: '', indirizzoLink: '' },
+   { nome: 'Festa Privata - Matrimonio', data: '21/09/2024', luogo: '', link: '', indirizzo: '', indirizzoLink: '' },
 ];
+// { nome: 'Festa Società Sportiva', data: '07/07/2024', luogo: "Monsampolo", link: '', indirizzo: '', indirizzoLink: '' },
 
 function popolaEventi() {
     var eventContainer = document.getElementById('event-container');
