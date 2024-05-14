cef84d7f31b8bfca67b9be4eac72e1866de45f27
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2024-05-14 15:18:05 +0200

    Create event-giomas-script.js

---
+++ scripts/event-giomas-script.js
@@ -0,0 +1,53 @@
+// Dati degli eventi
+var arEventi = [   
+  { band: "StarLiga - Ligabue Tribute", nome: "Comizio - Venagrande", data: '01/06/2024', confermata: true, },
+  { band: "Jam Quintet - Italian Tribute", nome: "Kasì Bar", data: '02/06/2024', confermata: true, },
+  { band: "StarLiga - Ligabue Tribute", nome: "???", data: '07/06/2024',  confermata: false, },
+  { band: "Various Artist", nome: "RETROMANIA 80", data: '08/06/2024',  confermata: false, },
+  { band: "Level Up - party band", nome: "Festa di Sant'Antonio", data: '15/06/2024', confermata: true, luogo: 'Spelonga (AP)', link: '', indirizzo: '', indirizzoLink: '' },
+  { band: "StarLiga - Ligabue Tribute", nome: "Sestiere di Porta Maggiore", data: '21/06/2024',  confermata: false, },
+  { band: "Level Up - party band", nome: 'Festa di Case di Coccia', data: '29/06/2024', luogo: 'Case di Coccia - Folignano (AP)', link: '', indirizzo: 'Piazza di Via Ivrea, Case di Coccia (AP)', indirizzoLink: 'https://maps.app.goo.gl/Te7qTcCRs8eUL9B79' },
+  
+  { nome: 'Sagra della Tagliata', data: '18/07/2024', luogo: 'Piane di Morro - Folignano (AP)', link: '', indirizzo: 'Piazza Giovanni Paolo II, Piane di Morro (AP)', indirizzoLink: 'https://maps.app.goo.gl/o8VwZFkG4ZLWJ1qB9' },
+  
+  { nome: 'Piazzetta e Campari', data: '01/08/2024', luogo: "Sant'Egidio di Monsampolo", link: '', indirizzo: '', indirizzoLink: '' },
+  { nome: 'Festa Privata - Matrimonio', data: '21/09/2024', luogo: '', link: '', indirizzo: '', indirizzoLink: '' },
+];
+// { nome: 'Festa Società Sportiva', data: '07/07/2024', luogo: "Monsampolo", link: '', indirizzo: '', indirizzoLink: '' },
+
+function popolaEventi() {
+    var eventContainer = document.getElementById('event-container');
+
+    // Ottieni la data odierna per confrontarla con le date degli eventi
+    var oggi = new Date();
+
+    // Per ogni evento nell'array
+    arEventi.forEach(function(evento) {
+        // Converte la data nel formato corretto (MM/GG/YYYY)
+        var dataParts = evento.data.split('/');
+        var dataFormatted = dataParts[1] + '/' + dataParts[0] + '/' + dataParts[2];
+        var data = new Date(dataFormatted);
+       
+        // Verifica se la data è valida e se non è passata
+        if (!isNaN(data.getTime()) && (data >= oggi || data.getDate() === oggi.getDate())) {
+            // Costruisci direttamente il template CARD utilizzando la sintassi dei template string
+            var cardHTML = `
+               <div class="card link" style="flex:0 0 80%; padding: 10px; max-width: 100%; margin:0; display: flex; align-items:center; background-color:#ffffff22; cursor: ${evento.link ? 'pointer' : 'default'}" onclick="apriLink('${evento.link}')">
+                 <div style="flex: 1; text-align: center; padding:0 5px;">
+                     <div class="event-day" style="font-size: 20px;">${data.toLocaleString('default', { weekday: 'short' }).toUpperCase()}</div>
+                     <div class="event-date" style="font-size: 34px; font-weight: bold;">${data.getDate()}</div>
+                     <div class="event-month" style="font-size: 20px;">${data.toLocaleString('default', { month: 'short' }).toUpperCase()}</div>
+                 </div>
+                 <div style="flex: 7; text-align: center;">
+                     <div class="event-name" style="font-size: 20px; font-weight: bold; text-transform: uppercase;">${evento.nome}</div>
+                     <div class="event-location" style="font-size: 16px;">${evento.luogo}</div>
+                     ${evento.indirizzo ? `<a href="${evento.indirizzoLink}" class="event-address" style="font-size: 12px; margin-top:2px; cursor: pointer;" target="_blank">${evento.indirizzo}</a>` : ''}
+                 </div>
+             </div>
+            `;
+
+            // Aggiungi la CARD HTML al container degli eventi
+            eventContainer.insertAdjacentHTML('beforeend', cardHTML);
+        }
+    });
+}
