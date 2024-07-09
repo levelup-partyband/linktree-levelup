8978cef9f6de22d14eded9261b5d4b1e66441acb
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2024-07-09 18:41:18 +0200

    Update events-script.js

--- scripts/events-script.js
+++ scripts/events-script.js
@@ -1,11 +1,11 @@
 // Dati degli eventi
 var arEventi = [   
    // { nome: "Festa di Sant'Antonio", data: '15/06/2024', luogo: 'Spelonga (AP)', link: '', indirizzo: '', indirizzoLink: '' },
-   { nome: 'Festa di Case di Coccia', data: '29/06/2024', luogo: 'Case di Coccia - Folignano (AP)', link: '', indirizzo: 'Piazza di Via Ivrea, Case di Coccia (AP)', indirizzoLink: 'https://maps.app.goo.gl/Te7qTcCRs8eUL9B79' },
-   { nome: '"Settimana Santa" Biancorossa', data: '08/07/2024', luogo: 'Sestiere Piazzarola - Ascoli Piceno (AP)', link: '', indirizzo: 'Via della Cartiera, 1, 63100 Ascoli Piceno AP', indirizzoLink: 'https://maps.app.goo.gl/uqoh7dRdUKPyobdE7' },
-   { nome: 'Sagra della Tagliata', data: '18/07/2024', luogo: 'Piane di Morro - Folignano (AP)', link: '', indirizzo: 'Piazza Giovanni Paolo II, Piane di Morro (AP)', indirizzoLink: 'https://maps.app.goo.gl/o8VwZFkG4ZLWJ1qB9' },
-   { nome: 'Piazzetta e Campari', data: '01/08/2024', luogo: "Sant'Egidio di Monsampolo", link: '', indirizzo: "Contrada Sant'Egidio, 45 B, 63077 Monsampolo del Tronto AP", indirizzoLink: 'https://maps.app.goo.gl/sS3irn2B1tdxnTRs8' },
-   { nome: 'Festa in Campagna', data: '31/08/2024', luogo: 'Tassanare di Rosora (AN)', link: '', indirizzo: 'Tassanare di Rosora (AN)', indirizzoLink: 'https://maps.app.goo.gl/CpeHn3qmBQVEpsAj8' },
+   { nome: 'Festa di Case di Coccia', data: '29/06/2024', luogo: 'Case di Coccia - Folignano (AP)', link: 'https://www.facebook.com/events/2188491668194611/', indirizzo: 'Piazza di Via Ivrea, Case di Coccia (AP)', indirizzoLink: 'https://maps.app.goo.gl/Te7qTcCRs8eUL9B79' },
+   { nome: '"Settimana Santa" Biancorossa', data: '08/07/2024', luogo: 'Sestiere Piazzarola - Ascoli Piceno (AP)', link: 'https://www.facebook.com/events/1003807131250235/', indirizzo: 'Via della Cartiera, 1, 63100 Ascoli Piceno AP', indirizzoLink: 'https://maps.app.goo.gl/uqoh7dRdUKPyobdE7' },
+   { nome: 'Sagra della Tagliata', data: '18/07/2024', luogo: 'Piane di Morro - Folignano (AP)', link: 'https://www.facebook.com/events/1272323350820389/', indirizzo: 'Piazza Giovanni Paolo II, Piane di Morro (AP)', indirizzoLink: 'https://maps.app.goo.gl/o8VwZFkG4ZLWJ1qB9' },
+   { nome: 'Piazzetta e Campari', data: '01/08/2024', luogo: "Sant'Egidio di Monsampolo", link: 'https://www.facebook.com/events/320271224484642/', indirizzo: "Contrada Sant'Egidio, 45 B, 63077 Monsampolo del Tronto AP", indirizzoLink: 'https://maps.app.goo.gl/sS3irn2B1tdxnTRs8' },
+   { nome: 'Festa in Campagna', data: '31/08/2024', luogo: 'Tassanare di Rosora (AN)', link: 'https://www.facebook.com/FestaTassanare', indirizzo: 'Tassanare di Rosora (AN)', indirizzoLink: 'https://maps.app.goo.gl/CpeHn3qmBQVEpsAj8' },
    { nome: 'Festa Privata - Matrimonio', data: '21/09/2024', luogo: '', link: '', indirizzo: '', indirizzoLink: '' },
 ];
 // { nome: 'Festa Società Sportiva', data: '07/07/2024', luogo: "Monsampolo", link: '', indirizzo: '', indirizzoLink: '' },
