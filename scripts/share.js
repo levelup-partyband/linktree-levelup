16c3d5041819146dedbe22cd1a0bf1a20c24b757
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2024-10-04 11:49:29 +0200

    Update share.js

--- scripts/share.js
+++ scripts/share.js
@@ -1,19 +1,39 @@
-// Controlla se il browser supporta l'API di condivisione
-const shareButton = document.getElementById('shareButton');
+// // Controlla se il browser supporta l'API di condivisione
+// const shareButton = document.getElementById('shareButton');
 
-shareButton.addEventListener('click', async () => {
-   if (navigator.share) {
-      try {
-         await navigator.share({
-            title: 'Level Up - party band',
-            text: 'Scopri la nostra band, clicca qui per maggiori informazioni!',
-            url: window.location.href // Condivide l'URL della pagina corrente
-         });
-         console.log('Contenuto condiviso con successo');
-      } catch (error) {
-         console.error('Errore durante la condivisione:', error);
+// shareButton.addEventListener('click', async () => {
+//    if (navigator.share) {
+//       try {
+//          await navigator.share({
+//             title: 'Level Up - party band',
+//             text: 'Scopri la nostra band, clicca qui per maggiori informazioni!',
+//             url: window.location.href // Condivide l'URL della pagina corrente
+//          });
+//          console.log('Contenuto condiviso con successo');
+//       } catch (error) {
+//          console.error('Errore durante la condivisione:', error);
+//       }
+//    } else {
+//       alert('La condivisione non è supportata su questo dispositivo o browser');
+//    }
+// });
+document.addEventListener('DOMContentLoaded', () => {
+   const shareButton = document.getElementById('shareButton');
+
+   shareButton.addEventListener('click', async () => {
+      if (navigator.share) {
+         try {
+            await navigator.share({
+               title: 'Level Up - party band',
+               text: 'Scopri la nostra band, clicca qui per maggiori informazioni!',
+               url: window.location.href // Condivide l'URL della pagina corrente
+            });
+            console.log('Contenuto condiviso con successo');
+         } catch (error) {
+            console.error('Errore durante la condivisione:', error);
+         }
+      } else {
+         alert('La condivisione non è supportata su questo dispositivo o browser');
       }
-   } else {
-      alert('La condivisione non è supportata su questo dispositivo o browser');
-   }
+   });
 });
