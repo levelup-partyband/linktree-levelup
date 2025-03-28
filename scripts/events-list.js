297b79d0aa41d08f3daffbe636612d6378194029
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2025-03-28 09:34:55 +0100

    Update events-list.js

--- scripts/events-list.js
+++ scripts/events-list.js
@@ -1,4 +1,10 @@
-var arEventi = [   
+let arEventi = [   
+   { nome: 'Sagra delle Crepes 2025', data: '08/06/2025', luogo: 'Chiesa dei Frati - Ascoli Piceno (AP)', link: '', indirizzo: 'Via Assisi, 2, 63100 Ascoli Piceno AP', indirizzoLink: 'https://g.co/kgs/nxZtRpD' },
+   { nome: 'Festa Privata - Matrimonio', data: '14/06/2025', luogo: 'Marina Palmense (FM)', link: '', indirizzo: '', indirizzoLink: '' },
+   { nome: 'Festa', data: '21/06/2025', luogo: 'Melfi (PZ)', link: '', indirizzo: '', indirizzoLink: '' },
+];
+
+let arEventi2024 = [
    // { nome: "Festa di Sant'Antonio", data: '15/06/2024', luogo: 'Spelonga (AP)', link: '', indirizzo: '', indirizzoLink: '' },
    { nome: 'Sagra di Case di Coccia', data: '29/06/2024', luogo: 'Case di Coccia - Folignano (AP)', link: 'https://www.facebook.com/events/2188491668194611/', indirizzo: 'Piazza di Via Ivrea, Case di Coccia (AP)', indirizzoLink: 'https://maps.app.goo.gl/Te7qTcCRs8eUL9B79' },
    { nome: '"Settimana Santa" Biancorossa', data: '08/07/2024', luogo: 'Sestiere Piazzarola - Ascoli Piceno (AP)', link: 'https://www.facebook.com/events/1003807131250235/', indirizzo: 'Via della Cartiera, 1, 63100 Ascoli Piceno AP', indirizzoLink: 'https://maps.app.goo.gl/uqoh7dRdUKPyobdE7' },
@@ -9,7 +15,4 @@ var arEventi = [
    { nome: 'Maxelâ', data: '06/09/2024', luogo: 'Ascoli Piceno', link: 'https://www.facebook.com/share/Bhg8hdHitSsT958E/', indirizzo: 'Via Tornasacco, 7, 63100 Ascoli Piceno AP', indirizzoLink: 'https://maps.app.goo.gl/tBGiskCGwXKSiWry7' },
    { nome: 'Festa Privata - Matrimonio', data: '21/09/2024', luogo: 'Monteprandone (AP)', link: '', indirizzo: '', indirizzoLink: '' },
    { nome: 'Capodanno 2025', data: '31/12/2024', luogo: 'Albergo Ristorante Terme - Acquasanta Terme (AP)', link: 'https://www.facebook.com/events/573668735379455/', indirizzo: 'P.za Terme, 20, 63095 Acquasanta Terme (AP)', indirizzoLink: 'https://maps.app.goo.gl/b1e1UpB2zoqSAy7n8' },
-   { nome: 'SAgra delle Crepes 2025', data: '08/06/2025', luogo: 'Chiesa dei Frati - Ascoli Piceno (AP)', link: '', indirizzo: 'Via Assisi, 2, 63100 Ascoli Piceno AP', indirizzoLink: 'https://g.co/kgs/nxZtRpD' },
-   { nome: 'Festa Privata - Matrimonio', data: '14/06/2025', luogo: 'Marina Palmense (FM)', link: '', indirizzo: '', indirizzoLink: '' },
-   { nome: 'Festa', data: '21/06/2025', luogo: 'Melfi (PZ)', link: '', indirizzo: '', indirizzoLink: '' },
 ];
