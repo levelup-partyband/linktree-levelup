76c4c5ed04729d9d9a64938a944946732a8ca6a1
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2025-05-29 14:18:59 +0200

    Update events-list.js

--- scripts/events-list.js
+++ scripts/events-list.js
@@ -1,11 +1,12 @@
 let arEventi = [   
+   { nome: "Quel Filo che ci Unisce", data: "02/06/2025", luogo: "Sant'Egidio alla Vibrata (TE)", link: "https://www.facebook.com/events/681740024674212", indirizzo: "Viale Abruzzi, 64016 Sant'Egidio alla Vibrata TE", indirizzoLink: "https://maps.app.goo.gl/8yDfzbYnwmheSMQJ6" },
    { nome: 'Sagra delle Crepes 2025', data: '08/06/2025', luogo: 'Chiesa dei Frati - Ascoli Piceno (AP)', link: 'https://www.facebook.com/events/689821517127706/', indirizzo: 'Via Assisi, 2, 63100 Ascoli Piceno AP', indirizzoLink: 'https://g.co/kgs/nxZtRpD' },
    { nome: 'Festa Privata - Matrimonio', data: '14/06/2025', luogo: 'Marina Palmense (FM)', link: '', indirizzo: '', indirizzoLink: '' },
    { nome: 'Moto Giro FMI', data: '21/06/2025', luogo: 'Melfi (PZ)', link: 'https://www.facebook.com/events/1203175651273608/', indirizzo: 'Via della Cittadinanza Attiva, 85025, Melfi PZ', indirizzoLink: 'https://maps.app.goo.gl/zE2mZGGLH64oPDHf8' },
+   { nome: 'Chalet Mimosa 45', data: '12/07/2025', luogo: 'Grottammare (AP)', link: 'https://www.facebook.com/events/689821517127706/', indirizzo: 'Lungomare della Repubblica, 45, 63066 Grottammare AP', indirizzoLink: 'https://maps.app.goo.gl/rA84smajfJGYoykT6' },
    { nome: '24° Sagra dei Maccheroncini alla Pagliarana', data: '31/07/2025', luogo: 'Pagliare del Tronto (AP)', link: 'https://www.facebook.com/events/687396143877688/', indirizzo: 'Piazza Kennedy, 63078 Pagliare del Tronto AP', indirizzoLink: 'https://maps.app.goo.gl/hrujjeCpyioHprJF6' },
    { nome: 'Ferragosto', data: '15/08/2025', luogo: 'Grottammare (AP)', link: 'https://www.facebook.com/events/664786599737380/', indirizzo: 'Piazza Pericle Fazzini, Grottammare AP', indirizzoLink: 'https://maps.app.goo.gl/owku9aeAHm6BPfxD7' },
 ];
-//    { nome: "-", data: "02/06/2025", luogo: "Sant'Egidio alla Vibrata (TE)", link: "", indirizzo: "", indirizzoLink: "" },
 let arEventi2024 = [
    // { nome: "Festa di Sant'Antonio", data: '15/06/2024', luogo: 'Spelonga (AP)', link: '', indirizzo: '', indirizzoLink: '' },
    { nome: 'Sagra di Case di Coccia', data: '29/06/2024', luogo: 'Case di Coccia - Folignano (AP)', link: 'https://www.facebook.com/events/2188491668194611/', indirizzo: 'Piazza di Via Ivrea, Case di Coccia (AP)', indirizzoLink: 'https://maps.app.goo.gl/Te7qTcCRs8eUL9B79' },
