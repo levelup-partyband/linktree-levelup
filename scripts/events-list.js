d71edbbbbebfe921c06ce56fafa6a8ea6a0a52f0
Level Up - party band <167519281+levelup-partyband@users.noreply.github.com>
2025-06-09 14:49:59 +0200

    Update events-list.js

--- scripts/events-list.js
+++ scripts/events-list.js
@@ -6,7 +6,7 @@ let arEventi = [
    { nome: 'Chalet Mimosa 45', data: '12/07/2025', luogo: 'Grottammare (AP)', link: 'https://www.facebook.com/events/689821517127706/', indirizzo: 'Lungomare della Repubblica, 45, 63066 Grottammare AP', indirizzoLink: 'https://maps.app.goo.gl/rA84smajfJGYoykT6' },
    { nome: '24° Sagra dei Maccheroncini alla Pagliarana', data: '31/07/2025', luogo: 'Pagliare del Tronto (AP)', link: 'https://www.facebook.com/events/687396143877688/', indirizzo: 'Piazza Kennedy, 63078 Pagliare del Tronto AP', indirizzoLink: 'https://maps.app.goo.gl/hrujjeCpyioHprJF6' },
    { nome: "Chalet Casablanca", data: '04/08/2024', luogo: "Porto d'Ascoli (AP)", link: '', indirizzo: '', indirizzoLink: '' },
-   { nome: 'Ferragosto', data: '15/08/2025', luogo: 'Grottammare (AP)', link: 'https://www.facebook.com/events/664786599737380/', indirizzo: 'Piazza Pericle Fazzini, Grottammare AP', indirizzoLink: 'https://maps.app.goo.gl/owku9aeAHm6BPfxD7' },
+   { nome: "Ferragosto", data: '15/08/2025', luogo: "Grottammare (AP)", link: 'https://www.facebook.com/events/664786599737380/', indirizzo: 'Piazza Pericle Fazzini, Grottammare AP', indirizzoLink: 'https://maps.app.goo.gl/owku9aeAHm6BPfxD7' },
    { nome: "Memorial", data: '30/08/2024', luogo: "Villa Marchesa, Sant'Edigio (TE)", link: '', indirizzo: '', indirizzoLink: '' },
 ];
 let arEventi2024 = [
